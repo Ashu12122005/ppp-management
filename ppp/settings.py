8aa7bba28e6a25781971c4a9c8a40947e79c45d7
Your Name <youremail@example.com>
2026-02-17 17:57:40 +0530

    add render csrf trusted origin

--- ppp/settings.py
+++ ppp/settings.py
@@ -22,6 +22,7 @@ ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
+    "https://ppp-management.onrender.com",
 ]
 
 # -------------------------------------------------
