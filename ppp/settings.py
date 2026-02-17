aea74e75257303c5876c641b9ce1fac685260488
Your Name <youremail@example.com>
2026-02-17 21:58:02 +0530

    Fix CSRF secure cookie issue

--- ppp/settings.py
+++ ppp/settings.py
@@ -25,6 +25,10 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8000",
     "https://ppp-management.onrender.com",
 ]
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 # -------------------------------------------------
 # APPLICATIONS
